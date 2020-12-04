94e7c96f37801a0baffffa9992974feffe51534e
Viktoria Cseke <44726497+FurfurV@users.noreply.github.com>
2020-12-04 20:20:27 +0000

    Add files via upload

---
+++ House_ai_prediction.java
@@ -0,0 +1,23 @@
+package com.example.house_ai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class House_ai_prediction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Never gonna give you up
+//        Never gonna let you down
+//        Never gonna run around and desert you
+//        Never gonna make you cry
+//        Never gonna say goodbye
+//        Never gonna tell a lie and hurt you
+
+
+    }
+}
